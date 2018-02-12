55743681ce01ccca9f8d90a127a0d093eebef3ba
Kayla Ondracek and Michael Albers <kayla@>
2018-02-12 15:55:18 -0800

    add tests and methods for checking if moves are valid horizontally, vertically, diagonally and also a method to move the queen to the attack space

--- QueenAttack.Tests/ModelTests/QueenAttackTest.cs
+++ QueenAttack.Tests/ModelTests/QueenAttackTest.cs
@@ -51,9 +51,80 @@ namespace QueenAttack.Test
 
       //Assert
       Assert.AreEqual(canAttack, checkHoriz);
-
     }
+
+    [TestMethod]
+    public void CanAttackVertical_ReturnTrue_True()
+    {
+      //Arrange
+      bool canAttack = true;
+      Queen attackQueen = new Queen(3, 4);
+      Queen targetQueen = new Queen(3, 2);
+
+      //Act
+      bool checkVert = attackQueen.CanAttackVertical(attackQueen.GetX(), targetQueen.GetX());
+
+      //Assert
+      Assert.AreEqual(canAttack, checkVert);
+    }
+
+    [TestMethod]
+    public void CanAttackDiagonal_ReturnTrue_True()
+    {
+      //Arrange
+      bool canAttack = true;
+      Queen attackQueen = new Queen(4, 4);
+      Queen targetQueen = new Queen(1, 7);
+
+      //Act
+      bool checkDiag = attackQueen.CanAttackDiagonal(attackQueen.GetX(), targetQueen.GetX(), attackQueen.GetY(), targetQueen.GetY());
+
+      //Assert
+      Assert.AreEqual(canAttack, checkDiag);
+    }
+
+    [TestMethod]
+    public void SetX_UpdateXAfterAttack()
+    {
+      //Arrange
+      Queen attackQueen = new Queen(4, 4);
+      Queen targetQueen = new Queen(1, 7);
+
+      //Act
+      attackQueen.SetX(targetQueen.GetX());
+
+      //Assert
+      Assert.AreEqual(attackQueen.GetX(), targetQueen.GetX());
+    }
+
+    [TestMethod]
+    public void SetY_UpdateYAfterAttack()
+    {
+      //Arrange
+      Queen attackQueen = new Queen(4, 4);
+      Queen targetQueen = new Queen(1, 7);
+
+      //Act
+      attackQueen.SetY(targetQueen.GetY());
+
+      //Assert
+      Assert.AreEqual(attackQueen.GetY(), targetQueen.GetY());
+    }
+
+    [TestMethod]
+    public void IsOnBoard_isPossumRelatedToRat_false()
+    {
+      //Arrange
+      Queen newQueen = new Queen(-1, 1);
+      bool onBoard = false;
+
+      //Act
+      bool thisQueenOnBoard = newQueen.IsOnBoard(newQueen.GetX(), newQueen.GetY());
+
+      //Assert
+      Assert.AreEqual(onBoard, thisQueenOnBoard);
+    }
+
+
   }
-
-
 }
--- QueenAttack/Models/Queen.cs
+++ QueenAttack/Models/Queen.cs
@@ -5,23 +5,33 @@ namespace QueenAttack.Models
 {
   public class Queen
   {
-    private int _XCoord;
-    private int _YCoord;
+    private int _xCoord;
+    private int _yCoord;
 
-    public Queen(int X, int Y)
+    public Queen(int x, int y)
     {
-      _XCoord = X;
-      _YCoord = Y;
+      _xCoord = x;
+      _yCoord = y;
     }
 
     public int GetX()
     {
-      return _XCoord;
+      return _xCoord;
+    }
+
+    public void SetX(int x)
+    {
+      _xCoord = x;
     }
 
     public int GetY()
     {
-      return _YCoord;
+      return _yCoord;
+    }
+
+    public void SetY(int y)
+    {
+      _yCoord = y;
     }
 
     public bool CanAttackHorizontal(int attackY, int targetY)
@@ -35,6 +45,45 @@ namespace QueenAttack.Models
         return false;
       }
     }
+
+    public bool CanAttackVertical(int attackX, int targetX)
+    {
+      if (attackX == targetX)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+
+    public bool CanAttackDiagonal(int attackX, int targetX, int attackY, int targetY)
+    {
+      int xDifference = Math.Abs(attackX - targetX);
+      int yDifference = Math.Abs(attackY - targetY);
+      if (xDifference == yDifference)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+
+    public bool IsOnBoard(int x, int y)
+    {
+      if (x < 0 || x > 8 || y < 0 || y > 8 )
+      {
+        return false;
+      }
+      else
+      {
+        return true;
+      }
+    }
+
 
   }
 }
